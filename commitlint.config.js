9b508234f12b5cca3564223f3e009cd8142dbe08
Lang-Cheng <lang_cl@outlook.com>
2021-01-10 20:21:54 +0800

    first commit

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: Lang Cheng
+ * @Date: 2021-01-10 19:14:16
+ * @LastEditTime: 2021-01-10 20:16:06
+ * @LastEditors: Lang Cheng
+ * @Description: commitlint
+ * @FilePath: \2048\commitlint.config.js
+ */
+module.exports = { extends: ['@commitlint/config-conventional'] };
